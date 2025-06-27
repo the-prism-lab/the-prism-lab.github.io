56694096ebb57a7a8254b9d693d2c7fc19452cd8
yilinearity <131245614+yilinearity@users.noreply.github.com>
2025-06-27 09:03:01 +0000

    Deploying to gh-pages from @ the-prism-lab/the-prism-lab.github.io@e564b2366b3314a6c5be479b8e25c7a2a301c174 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -168,12 +168,7 @@ ninja.data = [{
             window.location.href = "/blog/2015/silica/";
           
         },
-      },{id: "books-the-godfather",
-          title: 'The Godfather',
-          description: "",
-          section: "Books",handler: () => {
-              window.location.href = "/books/the_godfather/";
-            },},{id: "news-photo-induced-charge-transfer-in-g-quadruplexes-continues-to-fascinate-the-jacs-readers-after-our-first-g-quadruplex-paper-in-2013-doi-10-1021-ja407648d-photoinduced-charge-transfer-in-self-assembled-monodispersed-g-quadruplex-of-a-donor-acceptor-triad-was-highlighted-again-in-jacs-spotlight-doi-10-1021-jacs-5b02864-and-doi-10-1021-ja4093727-for-the-2013-paper-the-combined-transient-absorption-stimulated-raman-and-electron-paramagnetic-spectroscopies-suggested-the-hole-delocalization-in-the-h-bonded-cyclic-guanine-moieties-doi-10-1021-jacs-5b00977",
+      },{id: "news-photo-induced-charge-transfer-in-g-quadruplexes-continues-to-fascinate-the-jacs-readers-after-our-first-g-quadruplex-paper-in-2013-doi-10-1021-ja407648d-photoinduced-charge-transfer-in-self-assembled-monodispersed-g-quadruplex-of-a-donor-acceptor-triad-was-highlighted-again-in-jacs-spotlight-doi-10-1021-jacs-5b02864-and-doi-10-1021-ja4093727-for-the-2013-paper-the-combined-transient-absorption-stimulated-raman-and-electron-paramagnetic-spectroscopies-suggested-the-hole-delocalization-in-the-h-bonded-cyclic-guanine-moieties-doi-10-1021-jacs-5b00977",
           title: 'Photo-induced charge transfer in G-quadruplexes continues to fascinate the JACS readers. After our...',
           description: "",
           section: "News",},{id: "news-singlet-fission-in-multi-crystalline-diketopyrrolopyrrole-dpp-derivatives-observed-the-nice-work-of-pat-reports-one-of-the-first-observations-and-thorough-studies-of-singlet-fission-processes-in-dpp-materials-a-common-class-of-industrial-pigments-this-work-opens-a-new-direction-for-sf-study-doi-10-1021-acs-jpcb-5b10565",
@@ -254,87 +249,7 @@ ninja.data = [{
           section: "News",},{id: "news-we-ve-migrated-our-website-to-github-for-a-sleek-new-look-sparkles",
           title: 'We’ve migrated our website to GitHub for a sleek new look!  :sparkles:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
-            },},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
-        },
-      },{
+          section: "News",},{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
